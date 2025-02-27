705eb5921aee10c0a7f98ae7ed4d17a3f9f71d69
RPosam <ramposam88@gmail.com>
2025-02-27 18:39:46 +0530

    Application to generate configs which is being used to generate dags

--- helpers/constants.py
+++ helpers/constants.py
@@ -1,4 +1,4 @@
 yes_no_options = ["Yes","No"]
-bucket_list = ["rposam-devops-airflow"]
+bucket_list = ["rposam-etl-source-datasets-s3"]
 pipelines_options = ["Airflow with Snowflake","Airflow with POSTGRES","SNOWPIPE"]
 stage_name_options =["MIRROR_DB.MIRROR.STAGE_EXTERNAL_S3"]
